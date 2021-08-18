79ff6a37e333651483eb2b82008a467472311d0c
Brianna Bias <brianna.bias@gmail.com>
2021-08-18 16:04:18 -0700

    renamed mh crisis table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS crisis_lines;
+DROP TABLE IF EXISTS mh_crisis_lines;
 
-CREATE TABLE crisis_lines(
+CREATE TABLE mh_crisis_lines(
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     county TEXT NOT NULL,
     info TEXT NOT NULL
