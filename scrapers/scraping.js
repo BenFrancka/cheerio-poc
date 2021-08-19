a8ca67c857f0c7aa6de8784da8735cc1881bd040
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-08-19 13:10:07 -0700

    Tests are passing, working on cheerio scraping

--- scrapers/scraping.js
+++ scrapers/scraping.js
@@ -1,21 +1,45 @@
 import request from 'request';
 import cheerio from 'cheerio';
 
-request('https://namior.org/resources/community-resource-lists/county-mental-health-departments/', (error, response, html) => {
-  if (!error && response.statusCode == 200) {
+// request('https://namior.org/resources/community-resource-lists/county-mental-health-departments/', (error, response, html) => {
+//   if (!error && response.statusCode == 200) {
+//     const $ = cheerio.load(html);
+
+//     const theAnchors = $('p strong > a');
+//     const thePhones = $('p strong');
+//     const phoneString = thePhones.html();
+//     const phonesArray = phoneString.split('</a>');
+
+//     thePhones.toArray().forEach(item => console.log(item.children[1].data));
+    
+//     // console.log('-----------The Names-----------');
+//     // console.log(theAnchors.html());
+//     // console.log('-----------The Numbers-----------');
+//     // console.log(thePhones.text());
+//     // console.log('-----------The End-----------');
+//   }
+// });
+
+const URL = 'https://namior.org/resources/community-resource-lists/county-mental-health-departments/';
+
+console.log('hello world');
+const scrapeData = () => {
+
+  request(URL, (error, response, html) => {
     const $ = cheerio.load(html);
 
-    const theAnchors = $('p strong > a');
-    const thePhones = $('p strong');
-    const phoneString = thePhones.html();
-    const phonesArray = phoneString.split('</a>');
+    const child = 'a';
+    const parent = 'strong';
 
-    thePhones.toArray().forEach(item => console.log(item.children[1].data));
-    
-    // console.log('-----------The Names-----------');
-    // console.log(theAnchors.html());
-    // console.log('-----------The Numbers-----------');
-    // console.log(thePhones.text());
-    // console.log('-----------The End-----------');
-  }
-});
+    $(child, parent).each(
+      () => {
+        const data = {};
+        data.county = this.attribs.child;
+        data.info = this.attribs.parent;
+        console.log(child);
+      }
+    );
+  });
+};
+
+console.log(scrapeData);
